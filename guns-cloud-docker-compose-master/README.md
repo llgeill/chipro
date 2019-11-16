638cd6889a3f94c886a79a63d48dc6370453e6d6
liliguang <903857227@qq.com>
2019-11-16 09:57:49 +0800

    删除了docker-compose文件

--- guns-cloud-docker-compose-master/README.md
+++
@@ -1,5 +0,0 @@
-### 这是一键启动guns-cloud项目的利器
-
-### 使用前需要有docker知识储备，不了解docker的可以在https://www.stylefeng.cn/video/roses
-
-### 查看相关视频学习
--- guns-cloud-docker-compose-master/down_code.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd 3.guns-cloud-system
-git clone http://git.stylefeng.cn/guns-cloud/guns-cloud-system.git
-
-cd ../4.guns-cloud-gateway
-git clone http://git.stylefeng.cn/guns-cloud/guns-cloud-gateway.git
