887551293253b64f85b36a702fab725b3e08b138
liliguang <903857227@qq.com>
2019-11-19 10:17:49 +0800

    增加druid连接池配置

---
+++ chipro-core/core-pool/pool-druid/src/main/java/cn/spark/chipro/core/pool/druid/config/DruidConfig.java
@@ -0,0 +1,119 @@
+package cn.spark.chipro.core.pool.druid.config;
+
+import cn.spark.chipro.core.pool.druid.properties.DruidProperties;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.support.spring.stat.BeanTypeAutoProxyCreator;
+import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <h3>chipro</h3>
+ * <p>阿里巴巴druid連接池</p>
+ *
+ * @author : liliguang
+ * @date : 2019-11-19 09:30
+ **/
+@Configuration
+@ConditionalOnProperty(prefix = "spring.datasource", name = "url")
+public class DruidConfig {
+
+
+    /**
+     * druid配置
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DruidProperties druidProperties() {
+        return new DruidProperties();
+    }
+
+    /**
+     * druid数据库连接池
+     */
+    @Bean(initMethod = "init")
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        druidProperties().config(dataSource);
+        return dataSource;
+    }
+
+    /**
+     * druidServlet注册
+     */
+    @Bean
+    public ServletRegistrationBean druidServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new StatViewServlet());
+        registration.addUrlMappings("/druid/*");
+        return registration;
+    }
+
+    /**
+     * druid监控 配置URI拦截策略
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter(
+                "exclusions", "/static/*,*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid,/druid/*");
+        //用于session监控页面的用户名显示 需要登录后主动将username注入到session里
+        filterRegistrationBean.addInitParameter("principalSessionName", "username");
+        return filterRegistrationBean;
+    }
+
+    /**
+     * druid数据库连接池监控
+     */
+    @Bean
+    public DruidStatInterceptor druidStatInterceptor() {
+        return new DruidStatInterceptor();
+    }
+
+    /**
+     * 正则表达式定义切点
+     * @return
+     */
+    @Bean
+    public JdkRegexpMethodPointcut druidStatPointcut() {
+        JdkRegexpMethodPointcut druidStatPointcut = new JdkRegexpMethodPointcut();
+        String patterns = "cn.spark.chipro.*.service.*";
+        //可以set多个
+        druidStatPointcut.setPatterns(patterns);
+        return druidStatPointcut;
+    }
+
+    /**
+     * druid数据库连接池监控
+     */
+    @Bean
+    public BeanTypeAutoProxyCreator beanTypeAutoProxyCreator() {
+        BeanTypeAutoProxyCreator beanTypeAutoProxyCreator = new BeanTypeAutoProxyCreator();
+        beanTypeAutoProxyCreator.setTargetBeanType(DruidDataSource.class);
+        beanTypeAutoProxyCreator.setInterceptorNames("druidStatInterceptor");
+        return beanTypeAutoProxyCreator;
+    }
+
+    /**
+     * druid 为druidStatPointcut添加拦截
+     *
+     * @return
+     */
+    @Bean
+    public Advisor druidStatAdvisor() {
+        return new DefaultPointcutAdvisor(druidStatPointcut(), druidStatInterceptor());
+    }
+
+
+}
---
+++ chipro-core/core-pool/pool-druid/src/main/java/cn/spark/chipro/core/pool/druid/properties/DruidProperties.java
@@ -0,0 +1,122 @@
+package cn.spark.chipro.core.pool.druid.properties;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import lombok.Data;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * <h3>chipro</h3>
+ * <p>druid属性配置</p>
+ *
+ * @author : liliguang
+ * @date : 2019-11-19 09:56
+ **/
+@Data
+public class DruidProperties {
+
+    private String url;
+
+    private String username;
+
+    private String password;
+
+    private String driverClassName;
+
+    private Integer initialSize = 2;
+
+    private Integer minIdle = 1;
+
+    private Integer maxActive = 20;
+
+    private Integer maxWait = 60000;
+
+    private Integer timeBetweenEvictionRunsMillis = 60000;
+
+    private Integer minEvictableIdleTimeMillis = 300000;
+
+    private String validationQuery;
+
+    private Boolean testWhileIdle = true;
+
+    private Boolean testOnBorrow = false;
+
+    private Boolean testOnReturn = false;
+
+    private Boolean poolPreparedStatements = true;
+
+    private Integer maxPoolPreparedStatementPerConnectionSize = 20;
+
+    private String filters = "stat";
+
+    private String dataSourceName;
+
+    public void config(DruidDataSource dataSource) {
+
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setInitialSize(initialSize);     //定义初始连接数
+        dataSource.setMinIdle(minIdle);             //最小空闲
+        dataSource.setMaxActive(maxActive);         //定义最大连接数
+        dataSource.setMaxWait(maxWait);             //最长等待时间
+
+        // 配置间隔多久才进行一次检测，检测需要关闭的空闲连接，单位是毫秒
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+
+        // 配置一个连接在池中最小生存的时间，单位是毫秒
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        dataSource.setValidationQuery(getValidateQueryByUrl(url));
+        dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnReturn(testOnReturn);
+
+        // 打开PSCache，并且指定每个连接上PSCache的大小
+        dataSource.setPoolPreparedStatements(poolPreparedStatements);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+
+        try {
+            dataSource.setFilters(filters);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Properties createProperties() {
+        Properties properties = new Properties();
+        properties.put("url", this.url);
+        properties.put("username", this.username);
+        properties.put("password", this.password);
+        properties.put("driverClassName", this.driverClassName);
+        properties.put("initialSize", this.initialSize);
+        properties.put("maxActive", this.maxActive);
+        properties.put("minIdle", this.minIdle);
+        properties.put("maxWait", this.maxWait);
+        properties.put("poolPreparedStatements", this.poolPreparedStatements);
+        properties.put("maxPoolPreparedStatementPerConnectionSize", this.maxPoolPreparedStatementPerConnectionSize);
+        properties.put("validationQuery", getValidateQueryByUrl(this.url));
+        properties.put("testOnBorrow", this.testOnBorrow);
+        properties.put("testOnReturn", this.testOnReturn);
+        properties.put("testWhileIdle", this.testWhileIdle);
+        properties.put("timeBetweenEvictionRunsMillis", this.timeBetweenEvictionRunsMillis);
+        properties.put("minEvictableIdleTimeMillis", this.minEvictableIdleTimeMillis);
+        properties.put("filters", this.filters);
+        return properties;
+    }
+
+    private String getValidateQueryByUrl(String url) {
+        if (url.contains("oracle")) {
+            return "select 1 from dual";
+        } else if (url.contains("postgresql")) {
+            return "select version()";
+        } else if (url.contains("sqlserver")) {
+            return "select 1";
+        } else {
+            return "select 1";
+        }
+    }
+
+}
+
