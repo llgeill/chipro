e8ec4c87ea248588e2a9d276ff77265528ba8567
liliguang <903857227@qq.com>
2020-03-22 01:54:56 +0800

    webSocket整合成对外提供的Feign服务，方便其他服务推送消息

---
+++ chipro-websocket/chipro-websocket-api/src/main/java/cn/spark/chipro/websocket/api/constant/ServiceNameConstant.java
@@ -0,0 +1,10 @@
+package cn.spark.chipro.websocket.api.constant;
+
+/**
+ * 服务名称常量
+ */
+public interface ServiceNameConstant {
+
+    public String WEBSOCKET_SERVICE="chipro-websocket";
+
+}
---
+++ chipro-websocket/chipro-websocket-api/src/main/java/cn/spark/chipro/websocket/api/feign/WebSocketFeignService.java
@@ -0,0 +1,31 @@
+package cn.spark.chipro.websocket.api.feign;
+
+import cn.spark.chipro.core.result.Result;
+import cn.spark.chipro.websocket.api.constant.ServiceNameConstant;
+import cn.spark.chipro.websocket.api.model.vo.MessageVO;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * 用户对外服务
+ */
+@FeignClient(name = ServiceNameConstant.WEBSOCKET_SERVICE)
+@Component
+public interface WebSocketFeignService {
+
+    /**
+     * 消息推送
+     * @param message
+     * @return
+     */
+    @PostMapping("/push")
+    public Result push(@RequestBody MessageVO message);
+
+}
